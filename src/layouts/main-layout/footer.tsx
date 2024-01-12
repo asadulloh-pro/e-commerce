c823da3867ad0338d117649c5358c8db18b3a079
Asadulloh <asadullosadirdinov@gmail.com>
2024-01-12 23:34:48 +0500

    feat: add footer

--- src/layouts/main-layout/footer.tsx
+++ src/layouts/main-layout/footer.tsx
@@ -1,9 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
+
 const Footer = () => {
   return (
     <footer>
+      <div className="container mx-auto flex items-center justify-between py-[1rem]">
+        <Link href="/" className="text-[2rem] font-[700]">
+          E-COMMERCE
+        </Link>
 
+        <div className="flex justify-end items-center gap-[1rem]">
+          <Button>Telegram</Button>
+          <Button>Instagram</Button>
+          <Button>Youtbe</Button>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
