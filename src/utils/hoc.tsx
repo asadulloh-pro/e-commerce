d77622ba4589793d98877e88ea3b6d4e7b7f5292
Asadulloh <asadullosadirdinov@gmail.com>
2024-01-13 23:17:05 +0500

    add component error boundary

--- src/utils/hoc.tsx
+++ src/utils/hoc.tsx
@@ -1,9 +1,31 @@
-const hoc = () => {
-  return (
-    <div>
+import { ErrorBoundary } from '@/components/ui/error-boundary'
+import type { ComponentType } from 'react'
 
-    </div>
-  )
+export const hoc = function <SP, HP>(
+    hook: (props: Partial<SP>) => HP,
+    Component: ComponentType<HP & SP>,
+    displayName?: string
+) {
+    Component.displayName = displayName || 'HocWrapper'
+    const HocResult: any = (props: SP) => {
+        try {
+            const hookProps = hook(props) || ({} as any)
+            return (
+                <ErrorBoundary fallback={<p>Oops! Something went wrong!</p>}>
+                    <Component {...hookProps} {...props} />
+                </ErrorBoundary>
+            )
+        } catch (error) {
+            console.log('HocError', error)
+            return <p>Oops! Something went wrong!</p>
+        }
+    }
+
+    HocResult.Original = Component
+    HocResult.hook = hook
+
+    return HocResult as ComponentType<Partial<HP> & Partial<SP>> & {
+        Original: ComponentType<HP & SP>
+        hook: typeof hook
+    }
 }
-
-export default hoc;
